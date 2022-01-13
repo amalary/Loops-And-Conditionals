52242807a61e194464d72f380419d1e4dca95b56
Anthony Malary <amalary@bu.edu>
2022-01-12 17:31:34 -0800

    Getting to know you answered

--- Loops-And_Conditionals.js
+++ Loops-And_Conditionals.js
@@ -32,7 +32,8 @@ for(let i = 1; i < 21; i++){
 
 // 4. Write a javascript application that logs all numbers from 1 - 100 AND:
 /* 4a. If a number is divisible by 3 log "Fizz" instead of the number.
-   4b. If a number is divisible by 5 log "Buzz" instead of the number. */
+   4b. If a number is divisible by 5 log "Buzz" instead of the number.
+   4c. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz". */
 
 for(let i = 1; i< 101; i++){
     if (i % 3=== 0 && i % 5 === 0){
@@ -46,6 +47,43 @@ for(let i = 1; i< 101; i++){
     }
     else{
         console.log(i); 
-    }
-    
+    }  
 }; 
+
+//  5 Getting to Know You
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"]; 
+
+// 5a
+let kennyArray =kenny.splice(0,1,"Gameboy"); 
+
+console.log(kenny); 
+// 5b
+let jimClark2 = jimClark.splice(1,1,187); 
+
+console.log(jimClark); 
+// 5c
+let ryanArray = ryan.splice(2,1,"Gotham City"); 
+
+console.log(ryan); 
+
+// 5d
+
+let reubens = reuben.pop(); 
+
+let reubansHome = reuben.push('Chicago'); 
+
+console.log(reuben); 
+
+// 5e 
+
+let jimHaffCity = jimHaff.splice(2,1,"Los Angeles","Dallas","New York City"); 
+
+let jimHaffRemovedCity = jimHaff.splice(2,1); 
+
+console.log(jimHaff);
+
