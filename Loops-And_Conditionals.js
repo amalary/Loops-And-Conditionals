8be4687f821b3eab82dcdb921576e0dea3202c39
Anthony Malary <amalary@bu.edu>
2022-01-12 16:31:18 -0800

    Excited Kitten

--- Loops-And_Conditionals.js
+++ Loops-And_Conditionals.js
@@ -11,3 +11,22 @@ for(let i =0; i< 201; i++){
         console.log(i);
 }};
 
+/*  3. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+    3a.For every even number in your loop, log "...human...why you taking pictures of
+    me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+*/ 
+let catcPhrases = ["...human...why you taking pictures of me?...","...the catnip made me do it...", "...why does the red dot always get away..."]; 
+
+let meow = 0;
+for(let i = 1; i < 21; i++){
+    if(i %2 ===0){
+
+        meow = Math.floor(Math.random() * 3);
+        console.log(catcPhrases[meow]);
+    } 
+    else {
+        console.log(i);
+    }
+
+}; 
+
