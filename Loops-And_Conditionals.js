163d6f621f1c8370582bf87803c83428aecee2ba
Anthony Malary <amalary@bu.edu>
2022-01-12 16:40:07 -0800

    FizzBuzz answered

--- Loops-And_Conditionals.js
+++ Loops-And_Conditionals.js
@@ -30,3 +30,22 @@ for(let i = 1; i < 21; i++){
 
 }; 
 
+// 4. Write a javascript application that logs all numbers from 1 - 100 AND:
+/* 4a. If a number is divisible by 3 log "Fizz" instead of the number.
+   4b. If a number is divisible by 5 log "Buzz" instead of the number. */
+
+for(let i = 1; i< 101; i++){
+    if (i % 3=== 0 && i % 5 === 0){
+        console.log('FizzBuzz')
+    }
+    else if (i % 3===0){
+        console.log('Fizz');
+    }
+    else if (i % 5 ===0){
+        console.log('Buzz');
+    }
+    else{
+        console.log(i); 
+    }
+    
+}; 
