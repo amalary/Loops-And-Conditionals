d348f52bddd39d2fd0c9962695d08ee5b7ce608b
Anthony Malary <amalary@bu.edu>
2022-01-12 16:14:02 -0800

    Get even answer

--- Loops-And_Conditionals.js
+++ Loops-And_Conditionals.js
@@ -4,3 +4,10 @@ for(let i = 1; i < 21; i++){
     console.log(i); 
 } 
 
+// 2. Write a for loop that will log only the even numbers in 0 through 200.
+
+for(let i =0; i< 201; i++){
+    if (i % 2===0 ){
+        console.log(i);
+}};
+
